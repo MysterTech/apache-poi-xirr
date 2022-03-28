8a08a9f20122fc0720468fc31b6099947ef43358
C M Sanjay Krishna <sanjay.krishna@mystertech.com>
2022-03-28 17:56:31 +0530

    [Add]boilerplate code

---
+++ src/test/java/com/example/apachepoixirr/ApachePoiXirrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apachepoixirr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachePoiXirrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
